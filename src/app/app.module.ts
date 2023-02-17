02177326161ceb56a5ebfe82ec68b3a165e0eeb3
yeison2017 <yha.big@gmail.com>
2023-02-17 11:52:21 -0500

    Se crea componente para mostrar el header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,12 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/header/pages/home/home.component';
+import { ProductsHeaderComponent } from './components/header/pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './components/header/pages/home/components/filters/filters.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
