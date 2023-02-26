3faf6556ffb694639e98e76edfac5602627a7d1f
yeison2017 <yha.big@gmail.com>
2023-02-26 17:31:40 -0500

    Se cambian los nombre de las paginas

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Cart, CartItem } from "src/app/models/cart.model";
+import { CartService } from "../../services/cart.service";
 
 @Component({
   selector: "app-cart",
@@ -35,13 +36,13 @@ export class CartComponent implements OnInit {
     "action",
   ];
 
+  constructor(private cartService: CartService) {}
+
   ngOnInit(): void {
     this.dataSource = this.cart.items;
   }
 
   getTotal(items: Array<CartItem>): number {
-    return items
-      .map((item) => item.price * item.quantity)
-      .reduce((prev, current) => prev + current, 0);
+    return this.cartService.getTotal(items);
   }
 }
