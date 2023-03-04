38337196b268668f6ec5504e7c8314dba7e12106
yeison2017 <yha.big@gmail.com>
2023-03-04 15:45:45 -0500

    Filtra por categoria de los productos

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,15 +1,40 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  OnDestroy,
+} from "@angular/core";
+import { Subscription } from "rxjs";
+import { StoreService } from "../../../../services/store.service";
 
 @Component({
   selector: "app-filters",
   templateUrl: "./filters.component.html",
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCategory = new EventEmitter<string>();
+  categoriesSubscription: Subscription | undefined;
 
-  categories = ["shoes", "sports"];
+  categories: Array<string> | undefined;
+
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService
+      .getAllCategories()
+      .subscribe((response) => {
+        this.categories = response;
+      });
+  }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
