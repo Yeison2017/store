58a6892988783ca33d032332670ad1e81b55dc8f
yeison2017 <yha.big@gmail.com>
2023-03-05 15:01:49 -0500

    Se agregar más campos para la compra de productos

--- server/server.js
+++ server/server.js
@@ -15,6 +15,20 @@ const stripe = require("stripe")(
 app.post("/checkout", async (req, res, next) => {
   try {
     const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: { allowed_countries: ["US", "CA"] },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: { amount: 0, currency: "usd" },
+            display_name: "Free shipping",
+            delivery_estimate: {
+              minimum: { unit: "business_day", value: 5 },
+              maximum: { unit: "business_day", value: 7 },
+            },
+          },
+        },
+      ],
       line_items: req.body.items.map((item) => ({
         price_data: {
           currency: "usd",
